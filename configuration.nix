0327cd5b20c140cbc06942fd9dcd82bdc6233c56
Gaurav Juvekar <gauravjuvekar@gmail.com>
2023-11-12 21:44:29 -0800

    pkgs: install git

--- configuration.nix
+++ configuration.nix
@@ -63,6 +63,10 @@
 
   networking.firewall.enable = false;
 
+  environment.systemPackages = with pkgs;
+    [
+      gitMinimal
+    ];
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
